c20d5a77c5aeeab807f68c0fb1f61dedb4562cf1
ismailmare <imare@ualberta.ca>
2018-02-13 14:54:23 -0700

    Adding function

--- cmput_496/Assignment2/Go2/gtp_connection_go2.py
+++ cmput_496/Assignment2/Go2/gtp_connection_go2.py
@@ -29,9 +29,12 @@ class GtpConnectionGo2(gtp_connection.GtpConnection):
         """
         gtp_connection.GtpConnection.__init__(self, go_engine, board, outfile, debug_mode)
         self.commands["go_safe"] = self.safety_cmd
-        self.argmap["go_safe"] = (1, 'Usage: go_safe {w,b}')
         self.commands["timelimit"] = self.timelimit
+        self.commands["genmove"] = self.genmove
+
+        self.argmap["go_safe"] = (1, 'Usage: go_safe {w,b}')
         self.argmap["timelimit"] = (1, 'Usage: timelimit {seconds}')
+        self.argmap["genmove"] = (1, 'Usage: genmove {player}')
         self.timelimit = 1
 
     def safety_cmd(self, args):
@@ -47,6 +50,13 @@ class GtpConnectionGo2(gtp_connection.GtpConnection):
             self.respond('Error: {}'.format(str(e)))
 
     def timelimit(self,args):
+        """
+        This command sets the maximum time to use for all following genmove or solve commands
+
+        Arguments
+        ---------
+        args[0] : {seconds}
+        """
         try:
             if(args[0].isalpha()):
                 raise ValueError("Value Is Not A Number")
@@ -59,3 +69,49 @@ class GtpConnectionGo2(gtp_connection.GtpConnection):
             self.timelimit = seconds
         except Exception as e:
             self.respond('Error: {}'.format(str(e)))
+
+
+    def genmove(self,args):
+        #original code from util
+        
+        """
+        generate a move for the specified color
+
+        Arguments
+        ---------
+        args[0] : {'b','w'}
+            the color to generate a move for
+            it gets converted to  Black --> 1 White --> 2
+            color : {0,1}
+            board_color : {'b','w'}
+        """
+        try:
+            board_color = args[0].lower()
+            color = GoBoardUtil.color_to_int(board_color)
+            self.debug_msg("Board:\n{}\nko: {}\n".format(str(self.board.get_twoD_board()),
+                                                          self.board.ko_constraint))
+            move = self.go_engine.get_move(self.board, color)
+            if move is None:
+                self.respond("pass")
+                return
+
+            if not self.board.check_legal(move, color):
+                move = self.board._point_to_coord(move)
+                board_move = GoBoardUtil.format_point(move)
+                self.respond("Illegal move: {}".format(board_move))
+                raise RuntimeError("Illegal move given by engine")
+
+            # move is legal; play it
+            self.board.move(move,color)
+
+            self.debug_msg("Move: {}\nBoard: \n{}\n".format(move, str(self.board.get_twoD_board())))
+            move = self.board._point_to_coord(move)
+            board_move = GoBoardUtil.format_point(move)
+            self.respond(board_move)
+        except Exception as e:
+            self.respond('Error: {}'.format(str(e)))
+    
+
+    dev solve(self,player,timelimt):
+        # Need to implement
+        pass
